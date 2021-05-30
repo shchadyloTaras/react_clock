74ff5746765933f031b9cc91bc9b1e95fc7aae16
Taras Shchadylo <tarasdidg@gmail.com>
2021-05-30 15:59:39 +0300

    Updates

---
+++ precache-manifest.216c8b74bcafe371abe039b98c380969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71be051ee1f29b20b09f20de9d0d2d7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed2fa1b51696b616e845",
+    "url": "./static/css/main.e088abff.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2fa1b51696b616e845",
+    "url": "./static/js/main.83d1c381.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
